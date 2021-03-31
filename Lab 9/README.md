b2d1da6d1e580bb03631b426930bee8bbe690a0a
Sai Ankit <saiankit30@gmail.com>
2021-03-31 08:59:10 +0530

    add Lab 9 codes

---
+++ Lab 9/README.md
@@ -0,0 +1,5 @@
+Q1 .Write a program to interface the stepper motor in clockwise rotation
+[Solution](q1.asm)
+
+Q2 .Write a program to interface the stepper motor in anti-clockwise rotation
+[Solution](q2.asm)
---
+++ Lab 9/q1.asm
@@ -0,0 +1,21 @@
+#start=stepper_motor.exe#
+jmp start
+datain db 0000_0011b
+db 0000_0110b
+db 0000_1100b
+db 0000_1001b
+START: MOV BX, offset datain
+
+MOV SI,0h
+NEXT_STEP:
+
+WAIT: IN AL,07H
+TEST AL,10000000b
+JZ WAIT
+MOV AL,[BX][SI]
+OUT 7,AL
+INC SI
+CMP SI,4
+JC NEXT_STEP
+MOV SI,0
+JMP NEXT_STEP
